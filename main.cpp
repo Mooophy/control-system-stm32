f6c90dcb6afae662ed71341619a78c4bf3afd2b9
Yue Wang <Moophyalan@gmail.com>
2015-11-27 10:43:20 +1300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 /**
   *****************************************************************************
   * @file    main.cpp
-  * @author  Yue Wang 12027710
-  * @version V1.0.0
+  * @author  Yue Wang
   * @date    03-June-2013
-  * @brief   For the Assignment 6, 159.270 Hardware Oriented Programming.
+  * @brief   PC part, using Qt
    ****************************************************************************
   */
 
